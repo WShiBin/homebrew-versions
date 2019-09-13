e954327a2e9d8645d5c9a1cfee65c98a3b0135dd
ShiBin <15519900807@qq.com>
2019-09-14 00:22:06 +0800

    add cmake

---
+++ cmake.rb
@@ -0,0 +1,54 @@
+class Cmake < Formula
+  desc "Cross-platform make"
+  homepage "https://www.cmake.org/"
+  url "https://github.com/Kitware/CMake/releases/download/v3.15.0/cmake-3.15.0.tar.gz"
+  sha256 "0678d74a45832cacaea053d85a5685f3ed8352475e6ddf9fcb742ffca00199b5"
+  head "https://cmake.org/cmake.git"
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "101f3acfabc261ecb46154e3adde3b91ec3362973f43a2728cd32cf0ff225001" => :mojave
+    sha256 "c4cbddb3843b80a320e0393218e1fa8f836c384153f83c4fbfd9b9cf84dd32a7" => :high_sierra
+    sha256 "d3f4246935c9a2c95cd934c3901bf85e1072c49d22a684997b126add5a566ebe" => :sierra
+  end
+
+  depends_on "sphinx-doc" => :build
+
+  # The completions were removed because of problems with system bash
+
+  # The `with-qt` GUI option was removed due to circular dependencies if
+  # CMake is built with Qt support and Qt is built with MySQL support as MySQL uses CMake.
+  # For the GUI application please instead use `brew cask install cmake`.
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --no-system-libs
+      --parallel=#{ENV.make_jobs}
+      --datadir=/share/cmake
+      --docdir=/share/doc/cmake
+      --mandir=/share/man
+      --sphinx-build=#{Formula["sphinx-doc"].opt_bin}/sphinx-build
+      --sphinx-html
+      --sphinx-man
+      --system-zlib
+      --system-bzip2
+      --system-curl
+    ]
+
+    # There is an existing issue around macOS & Python locale setting
+    # See https://bugs.python.org/issue18378#msg215215 for explanation
+    ENV["LC_ALL"] = "en_US.UTF-8"
+
+    system "./bootstrap", *args, "--", "-DCMAKE_BUILD_TYPE=Release"
+    system "make"
+    system "make", "install"
+
+    elisp.install "Auxiliary/cmake-mode.el"
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write("find_package(Ruby)")
+    system bin/"cmake", "."
+  end
+end
